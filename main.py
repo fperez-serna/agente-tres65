5e05afcb0fc57f782d427ca092279d9325e8f5d1
Fer <f.perezserna@gmail.com>
2026-04-29 16:22:35 -0600

    fix main.py completo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import openai
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -42,4 +44,4 @@ def chat():
     return {"reply": reply}
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
